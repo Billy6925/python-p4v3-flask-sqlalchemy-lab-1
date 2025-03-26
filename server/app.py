738000ea20a409cdbe6aa67d129e3bbdbd73ef1c
Billy Eskein <billyeskein@gmail.com>
2025-03-26 19:43:36 +0300

    Update server/models.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,37 @@ def index():
     return make_response(body, 200)
 
 # Add views here
-
+@app.route('/earthquakes/<int:id>',methods =['GET'])
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify({
+            "id": earthquake.id,
+            "magnitude": earthquake.magnitude,
+            "location": earthquake.location,
+            "year": earthquake.year
+        }), 200
+    else:
+        return jsonify({"message": f'Earthquake {id} not found.'}),404
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods =['GET'])
+def get_earthquakes_by_minimun_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >=magnitude).all()
+    if earthquakes:
+        response ={
+            "count": len(earthquakes),
+            "quakes": [
+                {
+            "id": earthquake.id,
+            "magnitude": earthquake.magnitude,
+            "location": earthquake.location,
+            "year": earthquake.year
+                }for earthquake in earthquakes
+            ]
+        }
+        return jsonify(response),200
+    else:
+        return jsonify({"count":0,"quakes":[]}),200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData,Integer,Column,Float,String
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
@@ -7,3 +7,18 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model,SerializerMixin):
+    __tablename__ = 'earthquakes'
+    id = Column(Integer(), primary_key =True)
+    magnitude =Column(Float())
+    location = Column(String())
+    year =Column(Integer())
+
+    def __init__(self,magnitude=0.0,location="",year=0):
+        self.magnitude = magnitude
+        self.location = location
+        self.year = year
+
+    def __repr__(self):
+        return f'Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}'
+
